6648f20e6a473c2a334324fc7c450af1ada3d420
drinkcocoa <theremingi@gmail.com>
2023-12-18 02:37:36 +0900

    MIC_DROP = Scene change
    mic drop to go other scenes.

---
+++ Assets/forest1/Scripts/MicDrop1.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MicDrop1 : MonoBehaviour
+{
+    public GameObject mic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "mic 1")
+        {
+            SceneManager.LoadScene("Pop");
+            Debug.Log("Pop");
+        }
+    }
+
+}
---
+++ Assets/forest1/Scripts/MicDrop2.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MicDrop2 : MonoBehaviour
+{
+    public GameObject mic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "mic 1")
+        {
+            SceneManager.LoadScene("Acoustic");
+            Debug.Log("Acoustic");
+        }
+    }
+
+}
